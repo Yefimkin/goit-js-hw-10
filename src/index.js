e1e488fe4a73cd091b33204cfa42386fd7a9b647
Denys Yefimkin <iefimkindenis@gmail.com>
2022-08-01 21:50:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ const debounce = require('lodash.debounce');
 import Notiflix from 'notiflix'
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    searchBox: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+    };
