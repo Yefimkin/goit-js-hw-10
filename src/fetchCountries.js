5af677a116a0c6f257c2fdf7d0c43154a0d4315e
Denys Yefimkin <iefimkindenis@gmail.com>
2022-08-02 17:17:48 +0300

    some code

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,8 @@
 export default function fetchCountries(name) { 
-    
-}
+    const BASE_URL = "https://restcountries.com/v3.1/name";
+    const options = `?fields=name,capital,population,flags,languages`;
+
+    return fetch(`${BASE_URL}/${name}${options}`)
+        .then(response => response.json())
+        .catch(error => console.log(error))
+    }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 const debounce = require('lodash.debounce');
 import Notiflix from 'notiflix'
+import fetchCountries from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -8,4 +9,14 @@ const refs = {
     searchBox: document.querySelector('#search-box'),
     countryList: document.querySelector('.country-list'),
     countryInfo: document.querySelector('.country-info'),
-    };
+};
+    
+refs.searchBox.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY))
+
+function onSearch(e) {
+    e.preventDefault();
+
+    const inputText = refs.searchBox.value.trim();
+    refs.countryList.innerHTML = "";
+    refs.countryInfo.innerHTML = "";
+}
